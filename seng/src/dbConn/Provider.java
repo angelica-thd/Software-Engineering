a9786c037616a336e27703d9648c61908d403b46
angelica-thd <ageliki.th@gmail.com>
2020-06-25 16:55:19 +0300

    ya

--- seng/src/dbConn/Provider.java
+++
@@ -1,7 +0,0 @@
-package dbConn;
-
-public interface Provider {
-    String username = "postgres";
-    String password = "postgresqlserver";
-    String connURL = "jdbc:postgresql://localhost:5432/SE";
-}
